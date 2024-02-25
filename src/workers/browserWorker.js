f135bbb48bed7ce0d53d5cdc533a16cba2a9241f
Evgeny Rusakov <shoomec@yandex.ru>
2024-02-25 09:56:59 +0500

    chore(base): добавить архивы с данными после парсинга

--- src/workers/browserWorker.js
+++ src/workers/browserWorker.js
@@ -9,7 +9,6 @@ process.on('message', async (msg) => {
     headless: true,
     defaultViewport: null,
     args: [
-      //'--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-infobars',
       '--window-position=0,0',
@@ -38,23 +37,11 @@ process.on('message', async (msg) => {
 
     const page = pages[0];
 
-    //await page.setViewport({ width: 1280, height: 800 });
-
     await page.setCookie({
       name: NAME_COOKIE_REGION,
       value: `${code}`,
       domain: COOKIE_DOMAIN,
     });
-
-    // await page.setExtraHTTPHeaders({
-    //   'user-agent':
-    //     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    //   'upgrade-insecure-requests': '1',
-    //   accept:
-    //     'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    //   'accept-encoding': 'gzip, deflate, br',
-    //   'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-    // });
 
     process.send({
       payload: {
